c7ccb2a1a4d7945a020b84e8178381ed0f06c8d4
Gustavo Bigardi <guhbigardi@hotmail.com>
2018-02-27 19:32:29 -0300

    alterando credenciais

--- Botflix/Botflix/Dialogs/BotflixDialog.cs
+++ Botflix/Botflix/Dialogs/BotflixDialog.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
 using System;
 using System.Threading.Tasks;
@@ -9,9 +8,9 @@ using System.Configuration;
 namespace Botflix.Dialogs
 {
     [Serializable]
-    [LuisModel("502bc406-379a-4d0a-a468-8c751465257d", "a53bef52e5b147f781e9bd6260f83f79")]
-    public class BotflixDialog : LuisDialog<object>
+    public class BotflixDialog : BaseLuisDialog<object>
     {
+       
         string qnaSubscriptionKey = ConfigurationManager.AppSettings["QnaSubscriptionKey"];
         string qnaKnowledgebaseId = ConfigurationManager.AppSettings["QnaKnowledgebaseId"];
 
